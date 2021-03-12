0bac694b1684e8862823f07a0f5e469fa2db2828
shelldi <79559492+shelldi@users.noreply.github.com>
2021-03-12 22:43:02 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -7,8 +7,8 @@ from pyrogram.errors import FloodWait
 from apscheduler.schedulers.background import BackgroundScheduler
 
 
-api_id = "1086018"   # Get it from my.telegram.org
-api_hash = "3c2f1a043c1a22d5b0af74b8268993d5"   # Get it from my.telegram.org
+api_id = 1086018  # Get it from my.telegram.org
+api_hash = ''   # Get it from my.telegram.org
 feed_url = "https://torrentgalaxy.to/rss?magnet&user=29"   # RSS Feed URL of the site.
 bot_token = "1616198536:AAHogyNYpS3pKBHAVcTdTeIMTkZIr-CaN7M"   # Get it by creating a bot on https://t.me/botfather
 log_channel = "1280918281"   # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.

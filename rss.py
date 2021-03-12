dc2d5df3e1ced45761446e10269a8bd349fdcb8d
shelldi <79559492+shelldi@users.noreply.github.com>
2021-03-12 21:40:14 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -11,7 +11,7 @@ api_id = "1086018"   # Get it from my.telegram.org
 api_hash = "3c2f1a043c1a22d5b0af74b8268993d5"   # Get it from my.telegram.org
 feed_url = "https://torrentgalaxy.to/rss?magnet&user=29"   # RSS Feed URL of the site.
 bot_token = "1616198536:AAHogyNYpS3pKBHAVcTdTeIMTkZIr-CaN7M"   # Get it by creating a bot on https://t.me/botfather
-log_channel = "-1001356386015"   # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.
+log_channel = "1356386015"   # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.
 check_interval = 5   # Check Interval in seconds.    
 max_instances = 5   # Max parallel instance to be used.
 if os.environ.get("ENV"):   # Add a ENV in Environment Variables if you wanna configure the bot via env vars.

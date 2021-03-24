3b7e12efd6e5c0ecc51f2ed4781d54419ae46dd8
shelldi <79559492+shelldi@users.noreply.github.com>
2021-03-24 15:32:45 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -6,36 +6,34 @@ from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from apscheduler.schedulers.background import BackgroundScheduler
 
-
-api_id = 1086018  # Get it from my.telegram.org
-api_hash = ''   # Get it from my.telegram.org
-feed_url = "https://torrentgalaxy.to/rss?magnet&user=29"   # RSS Feed URL of the site.
-bot_token = "1616198536:AAHogyNYpS3pKBHAVcTdTeIMTkZIr-CaN7M"   # Get it by creating a bot on https://t.me/botfather
-log_channel = "1280918281"   # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.
-check_interval = 5   # Check Interval in seconds.    
+session_name = "1AZWarzUBu2Sf9JIVZOO9DyEXt88ljyimQT2YmBwarRVOlvnm5EOXMw-_rWPc21nikKuax7QB-rSmXTa5YTx66-Q7U00EFyAs6My2CQV7lFAQOUnu6Uq-zb7nt7xRls4IWQDi2QfW98z0bI8kluPmNWS7YTbZbxX5ipuGfACSmN8ksRtKfwJ09XZMcnKHTaPfmJ5WvV8RvSOLVmMdWWNTQxc_vYiEiMJT102UkDszmZVdQZr_jbHywOsDmr6-4g4tW8ixgFR08swTnqYe1vzXg84XWkNZyyoQOoj49rXQFgtCeuWgIbphQ7SZc-O9nh4-ltXJGv6SsXnyftq2DaBbdzTjRPGpOXE="
+api_id = "3751415"   # Get it from my.telegram.org
+api_hash = "6547d1aaebb53eea6707945a48c4dd2f"   # Get it from my.telegram.org
+feed_url = ["https://torrentgalaxy.to/rss?magnet"]   # RSS Feed URL of the site.
+   # Get it by creating a bot on https://t.me/botfather
+log_channel = "-1001478632499"   # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.
+check_interval = 5  # Check Interval in seconds.  
 max_instances = 5   # Max parallel instance to be used.
 if os.environ.get("ENV"):   # Add a ENV in Environment Variables if you wanna configure the bot via env vars.
+  session_name= os.environ.get("SESSION_NAME")
   api_id = os.environ.get("APP_ID")
   api_hash = os.environ.get("API_HASH")
   feed_url = os.environ.get("FEED_URL")
-  bot_token = os.environ.get("BOT_TOKEN")
   log_channel = int(os.environ.get("LOG_CHANNEL", None))
-  check_interval = int(os.environ.get("INTERVAL", 5))
+  check_interval = int(os.environ.get("INTERVAL", 30))
   max_instances = int(os.environ.get("MAX_INSTANCES", 5))
 
 db = pickledb.load('rss.db', True)
 if db.get("feed_url") == None:
   db.set("feed_url", "*")
-app = Client("rss-bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+app = Client(session_name=session_name, api_id=api_id, api_hash=api_hash)
 
 def check_feed():
-    FEED = feedparser.parse(feed_url)
+  for furl in feed_url:
+    FEED = feedparser.parse(furl)
     entry = FEED.entries[0]
     if entry.id != db.get("feed_url"):
-      
-                   # ↓ Edit this message as your needs.
-      message = f"/Mirror {entry.link}"
-      
+      message = f"{entry.link}"
       try:
         app.send_message(log_channel, message)
         db.set("feed_url", entry.id)
